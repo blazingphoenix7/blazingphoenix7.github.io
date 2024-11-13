13dc783c5d50d13a2cb876c141af4d3ef5c32345
Aaryan Mehta <73230976+blazingphoenix7@users.noreply.github.com>
2024-11-13 00:16:44 -0500

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/Aaryan_Mehta_Resume.pdf";
+import pdf from "../../Assets/AaryanMehta_Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
