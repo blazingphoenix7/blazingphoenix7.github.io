8af7f3662f4749c05fcf509c73c6865d9afb4309
Aaryan Mehta <73230976+blazingphoenix7@users.noreply.github.com>
2024-11-12 23:51:16 -0500

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -16,9 +16,10 @@ function AboutCard() {
             A substantial portion of my technical skill set is dedicated to developing innovative applications with <span className="purple">Artificial Intelligence and Machine Learning.</span> 
             <br /><br /> {}
             <br /><br /> {}
-            I'm currently working as an AI developer at The Institute of Fine Arts in New York City where my primary duty is to develop a state-of-the-art live video multi-language subtitle generation system. This system is mainly powered by  my <span className="purple">Master's in Computer Science</span> from <span className="purple">New York University</span>, where I've taken courses like 
-            Artificial Intelligence, Machine Learning, Data Structures and Algorithms, Data Science, Database Management Systems, 
-            Operating Systems and Big Data.
+            I'm currently working as an <span className="purple"> AI developer at The Institute of Fine Arts in New York City </span> where I am developing a 
+            state-of-the-art <span className="purple"> AI video translation system in Python and PyTorch, delivering real-time subtitles in 100+ languages at 
+            99.7% accuracy while being 200x faster than traditional methods. </span>
+            This system is being deployed on AWS using Kubernetes for unmatched scalability, compute power, security and fault tolerance.
             <br /><br /> {}
             I'm currently pursuing my <span className="purple">Master's in Computer Science</span> from <span className="purple">New York University</span>, where I've taken courses like 
             Artificial Intelligence, Machine Learning, Data Structures and Algorithms, Data Science, Database Management Systems, 
